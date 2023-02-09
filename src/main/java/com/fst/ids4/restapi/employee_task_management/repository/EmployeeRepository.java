2b2ef99bd64b764e73f6974f1d5c8f474933a278
iheb <iheb.missaoui@etudiant-fst.utm.tn>
2023-02-09 12:41:18 +0100

    CRUD employee completed 90%

--- src/main/java/com/fst/ids4/restapi/employee_task_management/repository/EmployeeRepository.java
+++ src/main/java/com/fst/ids4/restapi/employee_task_management/repository/EmployeeRepository.java
@@ -1,7 +1,6 @@
 package com.fst.ids4.restapi.employee_task_management.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +9,6 @@ import com.fst.ids4.restapi.employee_task_management.models.Employee;
 
 @Repository
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
-    Employee findByFirstName(String firstName);
-    Optional<Employee>  findById(String id);
+    List<Employee> findByFirstName(String firstName);
     List<Employee> findByLastName(String lastName);
 }
